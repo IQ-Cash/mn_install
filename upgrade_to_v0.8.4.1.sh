ee41077de29c0732e5cfb5768a07f696c29741e6
IQ.CASH (official) <39273144+IQ-Cash@users.noreply.github.com>
2020-07-21 11:51:23 +0000

    Add upgrade script for v0.8.4.1

---
+++ upgrade_to_v0.8.4.1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Tested with Ubuntu 16.04, 18.04, 20.04
+
+set -o errexit
+
+echo "Stopping node..."
+sudo systemctl stop iqcash
+
+echo "Downloading new version..."
+sudo wget https://github.com/IQ-Cash/iqcash/releases/download/v0.8.4.1/iqcash-cli-linux.tar.gz --directory-prefix /home/iqcash/ -O /home/iqcash/iqcash-cli-linux-v0.8.4.1.tar.gz
+
+echo "Extracting..."
+sudo tar -xzvf /home/iqcash/iqcash-cli-linux-v0.8.4.1.tar.gz -C /home/iqcash/
+sudo chown -R iqcash:iqcash /home/iqcash/iqcash* && sudo chmod 755 /home/iqcash/iqcash*
+sudo rm /home/iqcash/iqcash-cli-linux-v0.8.4.1.tar.gz
+
+echo "Starting node..."
+sudo systemctl start iqcash
+
+echo "========================================="
+echo "Masternode upgrade to v0.8.4.1 completed!"
